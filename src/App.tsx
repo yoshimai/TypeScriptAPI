8710a8c1943363d46104e317dd3b81514cfbd6fc
yoshimai <117501051+yoshimai@users.noreply.github.com>
2022-11-20 16:59:34 +0900

    Todo追加、えらー確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import axios from "axios";
+import { useState } from "react";
+import { Todo } from "./Todo";
 import "./styles.css";
 
 export default function App() {
+  const [todos, setTodos] = useState<any>([]);
+
   const onClickFetchData = () => {
     axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res);
+      setTodos(res.data);
     });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userid={todo.userid}></Todo>
+      ))}
     </div>
   );
 }
